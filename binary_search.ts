54b3071cd96ba0c928d6daab818e4d55ae208a87
Yuri <87913263+yurifalves@users.noreply.github.com>
2023-08-28 00:06:01 -0300

    Create binary_search.ts

---
+++ binary_search.ts
@@ -0,0 +1,19 @@
+function binarySearch<T>(list: T[], item: T): number | null {
+    // O(log2 n)
+    let low: number = 0;
+    let high: number = list.length - 1;
+  
+    while (low <= high) {
+      let mid: number = Math.floor((low + high) / 2);
+      let guess: T = list[mid];
+      if (guess === item) {
+        return mid;
+      } else if (guess > item) {
+        high = mid - 1;
+      } else {
+        low = mid + 1;
+      }
+    }
+
+    return null;
+}
