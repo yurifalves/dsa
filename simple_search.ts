bb1a9c3b1872e077efd0227c2915383c45f556ab
Yuri <87913263+yurifalves@users.noreply.github.com>
2023-09-26 01:06:27 -0300

    Update simple_search.ts

--- simple_search.ts
+++ simple_search.ts
@@ -1,12 +1,12 @@
 function simpleSearch<T>(list: Array<T>, item: T): number | null {
-	// O(n)
-	for (let i: number = 0; i < list.length - 1; i++) {
-		if (list[i] === item) {
-			return i;
-		}
-	}
+  // O(n)
+  for (let i: number = 0; i < list.length - 1; i++) {
+    if (list[i] === item) {
+      return i;
+    }
+  }
 
-	return null;
+  return null;
 }
 
 const myList: Array<number> = [2, 8, 9, 13, 25];
