fef9bb78773feff5094c7bda9a6af80a502eb011
Yuri <87913263+yurifalves@users.noreply.github.com>
2023-09-26 00:50:41 -0300

    Create recursive_sum.ts

---
+++ recursion/recursive_sum.ts
@@ -0,0 +1,17 @@
+function recursiveSum(list: Array<number>): number {
+  if (list.length === 0) {
+    return 0;
+  } else {
+    const firstElement: number = list.shift() as number;
+    return firstElement + recursiveSum(list);
+  }
+}
+
+console.log(recursiveSum([7, 3, 16])); // 26
+/*
+recursiveSum([7, 3, 16])
+= 7 + recursiveSum([3, 16])
+= 7 + 3 + recursiveSum([16])
+= 7 + 3 + 16 + recursiveSum([])
+= 7 + 3 + 16 + 0
+*/
