34838e6e1044d053a75eb197cda27f4efd131b6e
Yuri <87913263+yurifalves@users.noreply.github.com>
2023-09-26 01:05:39 -0300

    Update binary_search.ts

--- binary_search.ts
+++ binary_search.ts
@@ -1,21 +1,21 @@
 function binarySearch(list: Array<number>, item: number): number | null {
-	// O(log n)
-	let low: number = 0;
-	let high: number = list.length - 1;
+  // O(log n)
+  let low: number = 0;
+  let high: number = list.length - 1;
 
-	while (low <= high) {
-		let mid: number = Math.floor((low + high) / 2);
-		let guess: number = list[mid];
-		if (guess === item) {
-			return mid;
-		} else if (guess > item) {
-			high = mid - 1;
-		} else {
-			low = mid + 1;
-		}
-	}
+  while (low <= high) {
+    let mid: number = Math.floor((low + high) / 2);
+    let guess: number = list[mid];
+    if (guess === item) {
+      return mid;
+    } else if (guess > item) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
 
-	return null;
+  return null;
 }
 
 const myList: Array<number> = [1, 3, 5, 7, 9];
